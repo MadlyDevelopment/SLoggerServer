7da0cd0b7394b1d27f8f858ef595d6a33f9c4724
MadlyCaptaingreen <nikofahl@gmail.com>
2022-04-17 03:07:34 +0200

    feat: add new unit test for convert byte array to object

--- SLoggerUnitTests/UnitTestTools.cs
+++ SLoggerUnitTests/UnitTestTools.cs
@@ -5,7 +5,12 @@ namespace SLogger_Unit_Tests;
 
 public class UnitTestTools
 {
-
+    /// <summary>
+    /// Test the method which
+    /// converts an object to an byte array
+    /// </summary>
+    /// <param name="obj">Test object used for conversion</param>
+    /// <param name="testResultData">The result which the test should output</param>
     [Test]
     [TestCase("Test", new byte[]{34,84,101,115,116,34})]
     [TestCase(12, new byte[]{49,50})]
@@ -13,5 +18,24 @@ public class UnitTestTools
     {
         var data = Tools.ConvertAnObjectToAByteArray(obj);
         Assert.AreEqual(testResultData, data);
+    }
+
+    /// <summary>
+    /// Test the method which
+    /// converts a byte array to an object
+    /// </summary>
+    [Test]
+    public void TestConvertByteArrayToObj()
+    {
+        var dataOne = Tools.ConvertByteArrayToObject(new byte[]{49,50});
+        var dataTwo = Tools.ConvertByteArrayToObject(new byte[]{34,84,101,115,116,34});
+        if (int.Parse(dataOne.ToString()!)  != 12)
+        {
+            Assert.Fail("Conversation from byte array to int failed");
+        }
+        if (dataTwo.ToString() != "Test")
+        {
+            Assert.Fail("Conversation from byte array to string failed");
+        }
     }
 }
